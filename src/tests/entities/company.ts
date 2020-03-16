49c52085f7ac9510d75bb67e26d863b5c1802f05
Indream Luo <indreamluo@qq.com>
2020-03-16 14:49:17 +0000

    Remove lib folder.

--- src/tests/entities/company.ts
+++ src/tests/entities/company.ts
@@ -1,6 +1,5 @@
 import { table, column, id, many } from "../../entity/decorators";
 import { Employee } from "./employee";
-import { EntitySet } from "../../entity/entity-set-domain";
 
 @table('Company')
 export class Company {
@@ -11,5 +10,5 @@ export class Company {
     Name!: string;
 
     @many(Employee)
-    Employees!: EntitySet<Employee>;
+    Employees!: Employee[];
 }
