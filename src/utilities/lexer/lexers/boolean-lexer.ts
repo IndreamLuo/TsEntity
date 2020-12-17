d21841b0dfa58a743b5ff181ddaee8df6db1f77a
Indream Luo <indreamluo@qq.com>
2020-12-17 10:11:28 +0000

    Add boolean lexer.

---
+++ src/utilities/lexer/lexers/boolean-lexer.ts
@@ -0,0 +1,12 @@
+import { ExpressionTreeNode } from "../expression-tree-node";
+import { Lexer } from "./lexer";
+
+export class BooleanLexer extends Lexer<Boolean> {
+    constructor (
+        name: string,
+        subLexers: (string | Lexer<any> | (() => Lexer<any>))[] | string,
+        onExpressionTreeNodeGenerated: (node: ExpressionTreeNode<Boolean>) => Boolean = node => node.Value && true || false
+    ) {
+        super(name, subLexers, onExpressionTreeNodeGenerated);
+    }
+}
