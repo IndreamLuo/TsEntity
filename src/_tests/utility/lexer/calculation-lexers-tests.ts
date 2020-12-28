8ad9206dabbd342a3b148d8aee634f8984353f14
Indream Luo <indreamluo@qq.com>
2020-12-28 19:05:25 +0000

    Simplify operators.

--- src/_tests/utility/lexer/calculation-lexers-tests.ts
+++ src/_tests/utility/lexer/calculation-lexers-tests.ts
@@ -1,10 +1,8 @@
-import { CalculationOperator } from "../../../utilities/enums/operators/calculation-operator";
-import { ComparisonOperator } from "../../../utilities/enums/operators/comparison-operator";
-import { ConditionOperator } from "../../../utilities/enums/operators/condition-operator";
 import { BasicLexers } from "../../../utilities/lexer/basic-lexers";
 import { CalculationLexers } from "../../../utilities/lexer/calculation-lexers";
 import { CalculationExpression } from "../../../utilities/lexer/expressions/calculation-expression";
 import { SelectFieldExpression } from "../../../utilities/lexer/expressions/select-field-expression";
+import { Operator } from "../../../utilities/types/operators";
 import { Assert } from "../../_framework/assert";
 import { test, tests } from "../../_framework/decorators";
 import { AssertLexer } from "./assert-lexer";
@@ -13,17 +11,16 @@ import { AssertLexer } from "./assert-lexer";
 export class CalculationLexersTests {
     @test()
     ParseOperatorScripts() {
-        let comparisonOperators: string[] = Object.values(ComparisonOperator);
-        let calculationOperators: string[] = Object.values(CalculationOperator);
-        let conditionOperators: string[] = [ConditionOperator.And, ConditionOperator.Or];
+        let comparisonOperators = [Operator.EqualTo, Operator.NotEqualTo, Operator.GreaterThan, Operator.LessThan, Operator.NoGreaterThan, Operator.NoLessThan];
+        let calculationOperators = [Operator.Plus, Operator.Minus, Operator.MultiplyBy, Operator.DividedBy];
+        let conditionOperators = [Operator.And, Operator.Or];
         let otherOperators = ['a', '++', '&&&', '%'];
 
         AssertLexer.CanParse(CalculationLexers.ComparisonOperator, ...comparisonOperators).forEach(result => {
             Assert.AreEqual(result.Parse.Expression, result.Script);
         });
         AssertLexer.CanParse(CalculationLexers.Operator, ...comparisonOperators).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Type, 'Comparison');
-            Assert.AreEqual(result.Parse.Expression!.Operator, result.Script);
+            Assert.AreEqual(result.Parse.Expression, result.Script);
         });
         AssertLexer.CannotParse(CalculationLexers.ComparisonOperator, ...calculationOperators);
         AssertLexer.CannotParse(CalculationLexers.ComparisonOperator, ...conditionOperators);
@@ -33,16 +30,14 @@ export class CalculationLexersTests {
             Assert.AreEqual(result.Parse.Expression, result.Script);
         });
         AssertLexer.CanParse(CalculationLexers.Operator, ...calculationOperators).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Type, 'Calculation');
-            Assert.AreEqual(result.Parse.Expression!.Operator, result.Script);
+            Assert.AreEqual(result.Parse.Expression, result.Script);
         });
         AssertLexer.CannotParse(CalculationLexers.CalculationOperator, ...comparisonOperators);
         AssertLexer.CannotParse(CalculationLexers.CalculationOperator, ...conditionOperators);
         AssertLexer.CannotParse(CalculationLexers.CalculationOperator, ...otherOperators);
         
         AssertLexer.CanParse(CalculationLexers.Operator, ...conditionOperators).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Type, 'Condition');
-            Assert.AreEqual(result.Parse.Expression!.Operator, result.Script);
+            Assert.AreEqual(result.Parse.Expression, result.Script);
         });
         AssertLexer.CannotParse(CalculationLexers.Operator, ...otherOperators);
     }
@@ -73,32 +68,27 @@ export class CalculationLexersTests {
             .forEach(value => values2.push(`(${value})`, `(${codeBreaks}${value})`, `(${value}${codeBreaks})`, `(${codeBreaks}${value}${codeBreaks})`));
 
         AssertLexer.CanParse(CalculationLexers.Calculation, boolean1, boolean2).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Type, 'Condition');
-            Assert.AreEqual(result.Parse.Expression!.Operator.Operator, ConditionOperator.Is);
+            Assert.AreEqual(result.Parse.Expression!.Operator, Operator.Is);
             Assert.AreEqual(result.Script, '' + result.Parse.Expression!.Left);
         });
         AssertLexer.CanParse(CalculationLexers.Calculation, boolean3).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Type, 'Condition');
-            Assert.AreEqual(result.Parse.Expression!.Operator.Operator, ConditionOperator.Is);
+            Assert.AreEqual(result.Parse.Expression!.Operator, Operator.Is);
             Assert.AreEqual(result.Script, (result.Parse.Expression!.Left as SelectFieldExpression).Identifier);
         });
         AssertLexer.CanParse(CalculationLexers.Calculation, number1, number2, number3, number4).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Type, 'Condition');
-            Assert.AreEqual(result.Parse.Expression!.Operator.Operator, ConditionOperator.Is);
+            Assert.AreEqual(result.Parse.Expression!.Operator, Operator.Is);
             Assert.AreEqual(parseFloat(result.Script.replace(BasicLexers.CodeBreak.RegExpForGlobalSearch, '')), result.Parse.Expression!.Left);
         });
         AssertLexer.CanParse(CalculationLexers.Calculation, string1, string2, string3).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Type, 'Condition');
-            Assert.AreEqual(result.Parse.Expression!.Operator.Operator, ConditionOperator.Is);
+            Assert.AreEqual(result.Parse.Expression!.Operator, Operator.Is);
             Assert.AreEqual(result.Script.substring(1, result.Script.length - 2), result.Parse.Expression!.Left);
         });
         AssertLexer.CannotParse(CalculationLexers.Calculation, number5, number6, number7, string4, string5);
         
         Assert.IsTrue(values1.length);
         AssertLexer.CanParse(CalculationLexers.Calculation, ...values1).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Operator, ConditionOperator.Prior);
-            Assert.AreEqual((result.Parse.Expression!.Left as CalculationExpression).Operator.Type, 'Condition');
-            Assert.AreEqual((result.Parse.Expression!.Left as CalculationExpression).Operator.Operator, ConditionOperator.Is);
+            Assert.AreEqual(result.Parse.Expression!.Operator, Operator.Prior);
+            Assert.AreEqual((result.Parse.Expression!.Left as CalculationExpression).Operator, Operator.Is);
         });
         Assert.IsTrue(values2.length);
         AssertLexer.CannotParse(CalculationLexers.Calculation, ...values2);
@@ -130,9 +120,8 @@ export class CalculationLexersTests {
         AssertLexer.CannotParse(CalculationLexers.Calculation, wrongSelectFields, empty);
 
         AssertLexer.CanParse(CalculationLexers.Calculation, ...values1).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Operator, ConditionOperator.Prior);
-            Assert.AreEqual((result.Parse.Expression!.Left as CalculationExpression).Operator.Type, 'Condition');
-            Assert.AreEqual((result.Parse.Expression!.Left as CalculationExpression).Operator.Operator, ConditionOperator.Is);
+            Assert.AreEqual(result.Parse.Expression!.Operator, Operator.Prior);
+            Assert.AreEqual((result.Parse.Expression!.Left as CalculationExpression).Operator, Operator.Is);
         });
         AssertLexer.CannotParse(CalculationLexers.Calculation, ...values2);
     }
@@ -149,74 +138,56 @@ export class CalculationLexersTests {
         let condition8 = '(a).b == 1 || !(a.b.c + 1) == 0 && a.c.d <= 5';
 
         AssertLexer.CanParse(CalculationLexers.Calculation, condition1, condition2, condition3).forEach(result => {
-            Assert.AreEqual(result.Parse.Expression!.Operator.Type, 'Comparison');
-            Assert.IsTrue(result.Parse.Expression!.Operator.Operator);
+            Assert.IsTrue(result.Parse.Expression!.Operator);
             Assert.IsTrue(result.Parse.Expression!.Left);
             Assert.IsTrue(result.Parse.Expression!.Right);
         });
 
         AssertLexer.IsParsedAs(CalculationLexers.Calculation, condition1, {
-            Operator: {
-                Type: 'Comparison',
-                Operator: ComparisonOperator.EqualTo
-            },
+            Operator: Operator.EqualTo,
             Left: {
-                Operator: {
-                    Type: 'Condition',
-                    Operator: ConditionOperator.Is
-                },
+                Operator: Operator.Is,
                 Left: {
                     Identifier: 'a',
                     Field: 'b'
                 }
             },
             Right: {
-                Operator: {
-                    Type: 'Condition',
-                    Operator: ConditionOperator.Is
-                },
+                Operator: Operator.Is,
                 Left: 1
             }
         });
 
         AssertLexer.IsParsedAs(CalculationLexers.Calculation, condition4, {
-            Operator: {
-                Type: 'Condition',
-                Operator: ConditionOperator.Not
-            },
+            Operator: Operator.Not,
             Left: {
-                Operator: {
-                    Type: 'Condition',
-                    Operator: ConditionOperator.Is
-                },
+                Operator: Operator.Is,
                 Left: true
             }
         });
 
         AssertLexer.IsParsedAs(CalculationLexers.Calculation, condition5, {
-            Operator: { Operator: ConditionOperator.And },
+            Operator: Operator.And,
             Left: {
-                Operator: { Operator: ConditionOperator.Is },
+                Operator: Operator.Is,
                 Left: { Identifier: 'a' }
             },
             Right: {
-                Operator: { Operator: CalculationOperator.Plus },
+                Operator: Operator.Plus,
                 Left: {
-                    Operator: { Operator: ConditionOperator.Is },
+                    Operator: Operator.Is,
                     Left: { Identifier: 'b' }
                 },
                 Right: {
-                    Operator: { Operator: CalculationOperator.MultiplyBy },
+                    Operator: Operator.MultiplyBy,
                     Left: {
-                        Operator: { Operator: ConditionOperator.Is },
+                        Operator: Operator.Is,
                         Left: { Identifier: 'c' }
                     },
                     Right: {
-                        Operator: { Operator: ConditionOperator.Not },
+                        Operator: Operator.Not,
                         Left: {
-                            Operator: {
-                                Operator: ConditionOperator.Is
-                            },
+                            Operator: Operator.Is,
                             Left: { Identifier: 'd' }
                         }
                     }
@@ -225,11 +196,11 @@ export class CalculationLexersTests {
         });
         
         AssertLexer.IsParsedAs(CalculationLexers.Calculation, condition6, {
-            Operator: { Operator: ComparisonOperator.GreaterThan },
+            Operator: Operator.GreaterThan,
             Left: {
-                Operator: { Operator: ConditionOperator.Not },
+                Operator: Operator.Not,
                 Left: {
-                    Operator: { Operator: ConditionOperator.Is },
+                    Operator: Operator.Is,
                     Left: {
                         Identifier: {
                             Identifier: 'my',
@@ -240,30 +211,30 @@ export class CalculationLexersTests {
                 }
             },
             Right: {
-                Operator: { Operator: ConditionOperator.Is },
+                Operator: Operator.Is,
                 Left: 18
             }
         });
         
         AssertLexer.IsParsedAs(CalculationLexers.Calculation, condition7, {
-            Operator: { Operator: ConditionOperator.Or },
+            Operator: Operator.Or,
             Left: {
-                Operator: { Operator: ComparisonOperator.EqualTo },
+                Operator: Operator.EqualTo,
                 Left: {
-                    Operator: { Operator: ConditionOperator.Is },
+                    Operator: Operator.Is,
                     Left: { Identifier: 'hello', Field: 'world' }
                 },
                 Right: {
-                    Operator: { Operator: ConditionOperator.Is },
+                    Operator: Operator.Is,
                     Left: true
                 }
             },
             Right: {
-                Operator: { Operator: ComparisonOperator.GreaterThan },
+                Operator: Operator.GreaterThan,
                 Left: {
-                    Operator: { Operator: ConditionOperator.Not },
+                    Operator: Operator.Not,
                     Left: {
-                        Operator: { Operator: ConditionOperator.Is },
+                        Operator: Operator.Is,
                         Left: {
                             Identifier: {
                                 Identifier: 'my',
@@ -274,17 +245,16 @@ export class CalculationLexersTests {
                     }
                 },
                 Right: {
-                    Operator: { Operator: ConditionOperator.Is },
+                    Operator: Operator.Is,
                     Left: 18
                 }
             }
         });
 
-        // '(a).b == 1 || !(a.b.c + 1) == 0 && a.c.d <= 5'
         AssertLexer.IsParsedAs(CalculationLexers.Calculation, condition8, {
-            Operator: { Operator: ConditionOperator.Or },
+            Operator: Operator.Or,
             Left: {
-                Operator: { Operator: ComparisonOperator.EqualTo },
+                Operator: Operator.EqualTo,
                 Left: {
                     Left: {
                         Identifier: 'a',
@@ -294,15 +264,15 @@ export class CalculationLexersTests {
                 Right: { Left: 1 }
             },
             Right: {
-                Operator: { Operator: ConditionOperator.And },
+                Operator: Operator.And,
                 Left: {
-                    Operator: { Operator: ComparisonOperator.EqualTo },
+                    Operator: Operator.EqualTo,
                     Left: {
-                        Operator: { Operator: ConditionOperator.Not },
+                        Operator: Operator.Not,
                         Left: {
-                            Operator: { Operator: ConditionOperator.Prior },
+                            Operator: Operator.Prior,
                             Left: {
-                                Operator: { Operator: CalculationOperator.Plus },
+                                Operator: Operator.Plus,
                                 Left: { Left: { Identifier: { Identifier: 'a', Field: 'b' }, Field: 'c' } },
                                 Right: { Left: 1 }
                             }
@@ -311,7 +281,7 @@ export class CalculationLexersTests {
                     Right: { Left: 0 }
                 },
                 Right: {
-                    Operator: { Operator: ComparisonOperator.NoGreaterThan },
+                    Operator: Operator.NoGreaterThan,
                     Left: { Left: { Identifier: { Identifier: 'a', Field: 'c' }, Field: 'd' } },
                     Right: { Left: 5 }
                 }
--- src/utilities/lexer/expressions/operator-expression.ts
+++
@@ -1,4 +0,0 @@
-export interface OperatorExpression {
-    Type: string;
-    Operator: string;
-}
